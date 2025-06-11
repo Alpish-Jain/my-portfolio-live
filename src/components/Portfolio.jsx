42983e45cfbd75af2e6add33248986b4d93ba39c
Alpish-Jain <46166671+Alpish-Jain@users.noreply.github.com>
2025-06-11 19:58:37 +0530

    fixing the about and portfolio sections

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -21,7 +21,7 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white py-12 pt-20 "
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white py-12 pt-100 md:pt-4 "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
