5af142fbbb69be544765839bd166b1606704b9ae
Alpish-Jain <46166671+Alpish-Jain@users.noreply.github.com>
2025-06-15 14:12:24 +0530

    Renaming the experience section to skills

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -99,13 +99,13 @@ const Experience = () => {
 
   return (
     <div
-      name="experience"
+      name="skills"
       className="bg-gradient-to-b from-gray-800 to-black w-full"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
-            Experience
+            Skills & Tools
           </p>
           <p className="py-6">Explore the diverse set of technologies I've utilized and built solutions with.</p>
         </div>
